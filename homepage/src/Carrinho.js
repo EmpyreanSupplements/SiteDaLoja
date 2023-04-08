52d3022620d7d39d7fd574413cde7b276f5b0397
Zion <santoszion4@gmail.com>
2023-04-08 12:31:37 -0300

    Nova Homepage do site

---
+++ homepage/src/Carrinho.js
@@ -0,0 +1,16 @@
+import NavbarComp from './Components/NavbarComp';
+import Footer from './Components/Footer';
+function Carrinho() {
+    return (  
+        <>
+        <NavbarComp />
+       <div className="Carrinho">
+    
+       <h1>Tela de Compra</h1>
+    
+       </div>
+       <Footer/>
+       </>
+)}
+
+export default Carrinho;
