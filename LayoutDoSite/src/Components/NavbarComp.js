857994e3462887dc18a03882d47eea526a333ad2
Giovana Joice <giovanajoice11@gmail.com>
2023-05-07 20:54:30 -0300

    nova versão da barra de pesquisa

--- LayoutDoSite/src/Components/NavbarComp.js
+++ LayoutDoSite/src/Components/NavbarComp.js
@@ -25,17 +25,20 @@ function NavbarComp() {
                   </Offcanvas.Title>
                 </Offcanvas.Header>
                 <Offcanvas.Body>
-                <Form className="d-flex">
-                    <Form.Control
-                      type="search"
-                      placeholder="Search"
-                      className="me-2"
-                      aria-label="Search"
-                    />
-                    <Button className='b' padding-bottom="20%" ><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-  <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-</svg></Button>
-                  </Form>
+                
+                <form class="form-inline my-2 my-lg-0">
+                  <div class="input-group rounded-pill">
+                    <input class="form-control border-0 rounded-pill" type="search" placeholder="Search..."/>
+                    <div class="input-group-append">
+                      <button class="btn btn-outline-secondary border-0 rounded-pill" type="submit">
+                        <img src="search.svg"/>
+                      </button>
+                    </div>
+                  </div>
+                </form>
+
+
+
                   <Nav className="justify-content-end flex-grow-1 pe-3">
                     <Nav.Link className='c' href="/">Home</Nav.Link>
                     <Nav.Link className='c' href="/Carrinho">Carrinho</Nav.Link>
